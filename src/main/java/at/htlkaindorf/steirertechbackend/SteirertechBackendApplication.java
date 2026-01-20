7b3fb2e788ee1b7ab543ab72e3f6b39bb03d61c2
harjab22 <harjab22@htl-kaindorf.at>
2026-01-20 15:03:04 +0100

    Init Commit

---
+++ src/main/java/at/htlkaindorf/steirertechbackend/SteirertechBackendApplication.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.steirertechbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SteirertechBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SteirertechBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/htlkaindorf/steirertechbackend/SteirertechBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package at.htlkaindorf.steirertechbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SteirertechBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
